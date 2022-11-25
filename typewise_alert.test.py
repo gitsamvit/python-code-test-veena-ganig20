27cdb479fd4f99fc2a05791a16d4cff4a2cf2738
veena-ganig20 <118328217+veena-ganig20@users.noreply.github.com>
2022-11-25 21:51:50 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -7,6 +7,14 @@ class TypewiseTest(unittest.TestCase):
     self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == 'TOO_LOW')
     self.assertTrue(typewise_alert.infer_breach(60, 30, 50) == 'TOO_HIGH')
     self.assertTrue(typewise_alert.infer_breach(20, 20, 20) == 'NORMAL')
+    
+  def test_classify_temperature_breach(self):
+    self.assertTrue(typewise_alert.Passive.classify_temperature_breach('PASSIVE-COOLING',20) == 'NORMAL')
+    self.assertTrue(typewise_alert.Passive.classify_temperature_breach('PASSIVE-COOLING',40) == 'TOO-HIGH')
+    self.assertTrue(typewise_alert.Active.classify_temperature_breach('ACTIVE-COOLING',50) == 'TOO-HIGH')
+    self.assertTrue(typewise_alert.Active.classify_temperature_breach('ACTIVE-COOLING',45) == 'NORMAL')
+    self.assertTrue(typewise_alert.Med.classify_temperature_breach('ACTIVE-COOLING',40) == 'NORMAL')
+    
 
   #def test_send_to_email(self):
     #self.assertTrue(typewise_alert.send_to_email('TOO_LOW') == 'Hi, the temperature is too low')
