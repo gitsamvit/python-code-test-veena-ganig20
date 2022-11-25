796aef172ad6bd612d3703c76832783f4b08c6f4
veena-ganig20 <118328217+veena-ganig20@users.noreply.github.com>
2022-11-25 07:18:13 +0530

    Update typewise_alert.py

--- typewise_alert.py
+++ typewise_alert.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 
 def infer_breach(value, lowerLimit, upperLimit):
   if value < lowerLimit:
@@ -6,10 +7,12 @@ def infer_breach(value, lowerLimit, upperLimit):
     return 'TOO_HIGH'
   return 'NORMAL'
 
-
-def classify_temperature_breach(coolingType, temperatureInC):
-  lowerLimit = 0
-  upperLimit = 0
+#create abstract class - context for the main class
+class ClassifyTemperature(ABC):
+  def classify_temperature_breach(coolingType, temperatureInC):
+    pass
+  
+  '''def classify_temperature_breach(coolingType, temperatureInC):
   if coolingType == 'PASSIVE_COOLING':
     lowerLimit = 0
     upperLimit = 35
@@ -20,11 +23,36 @@ def classify_temperature_breach(coolingType, temperatureInC):
     lowerLimit = 0
     upperLimit = 40
   return infer_breach(temperatureInC, lowerLimit, upperLimit)
+  '''
 
 
-def check_and_alert(alertTarget, batteryChar, temperatureInC):
+lowerLimit = 0
+upperLimit = 0
+class Passive(ClassifyTemperature):
+  def classify_temperature_breach(coolingType, temperatureInC):
+     if coolingType == 'PASSIVE_COOLING':
+        lowerLimit = 0
+        upperLimit = 35
+      return infer_breach(temperatureInC, lowerLimit, upperLimit)
+    
+ class Active(ClassifyTemperature):
+  def classify_temperature_breach(coolingType, temperatureInC):
+     if coolingType == 'HI_ACTIVE_COOLING':
+        lowerLimit = 0
+        upperLimit = 45
+      return infer_breach(temperatureInC, lowerLimit, upperLimit)
+
+ class Med(ClassifyTemperature):
+  def classify_temperature_breach(coolingType, temperatureInC):
+     if coolingType == 'MED_ACTIVE_COOLING':
+        lowerLimit = 0
+        upperLimit = 40
+      return infer_breach(temperatureInC, lowerLimit, upperLimit)
+    
+     
+  def check_and_alert(alertTarget, batteryChar, temperatureInC):
   breachType =\
-    classify_temperature_breach(batteryChar['coolingType'], temperatureInC)
+    ClassifyTemperature.classify_temperature_breach(batteryChar['coolingType'], temperatureInC)
   if alertTarget == 'TO_CONTROLLER':
     send_to_controller(breachType)
   elif alertTarget == 'TO_EMAIL':
