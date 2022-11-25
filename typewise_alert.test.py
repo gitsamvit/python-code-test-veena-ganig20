7090eb258f3348bad13fc1a7f9dc69beb03760af
veena-ganig20 <118328217+veena-ganig20@users.noreply.github.com>
2022-11-25 15:09:46 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -7,6 +7,10 @@ class TypewiseTest(unittest.TestCase):
     self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == 'TOO_LOW')
     self.assertTrue(typewise_alert.infer_breach(60, 30, 50) == 'TOO_HIGH')
     self.assertTrue(typewise_alert.infer_breach(20, 20, 20) == 'NORMAL')
+    
+  def test_send_to_email(self):
+    self.assertTrue(typewise_alert.send_to_email('TOO_LOW') == 'Hi, the temperature is too low')
+    self.assertTrue(typewise_alert.send_to_email('TOO_HIGH') == 'Hi, the temperature is too high')
 
 
 
